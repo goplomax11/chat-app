bd657beb36a3a212917cf8dc1c39cba0bed64416
igor <goplomax11@yandex.ru>
2024-04-14 16:57:58 +0300

    /auth/signUp

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,42 @@
-export const signUp = (req, res) => {
+import User from "../models/user.model.js";
+
+export const signUp = async (req, res) => {
   try {
-    const { fullName, username, password, confirmPassword, gender } = res.body;
-  } catch (error) {}
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    console.log(password);
+    console.log(confirmPassword);
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: " Username already exists" });
+    }
+
+    const profilePicture = `https://robohash.org/${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePicture,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePicture: newUser.profilePicture,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 export const login = (req, res) => {
--- server/server.js
+++ server/server.js
@@ -4,13 +4,11 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Server is ready!");
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
